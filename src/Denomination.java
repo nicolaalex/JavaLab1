7eb43a68aabcd7cc776164cb7b0dad2d2bb0675a
nicolalex <nicola@github>
2025-04-21 00:03:28 -0500

    Fixed layout issue where smaller denominations overlapped larger ones.

--- src/Denomination.java
+++ src/Denomination.java
@@ -1,6 +1,30 @@
+public class Denomination {
+    private final String name;
+    private final double amount;
+    private final String type;
+    private final String img;
 
-// record for denomination
-public record Denomination(String name, double amt, String form, String img ) {
+    public Denomination(String name, double amount, String type, String img) {
+        this.name = name;
+        this.amount = amount;
+        this.type = type;
+        this.img = img;
+    }
 
+    public double amt() { return amount; }
+    public String name() { return name; }
+    public String img() { return img; }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Denomination)) return false;
+        Denomination d = (Denomination) o;
+        return Double.compare(d.amount, amount) == 0 && name.equals(d.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
